201732272dee4817650418c8cb1fb0cea7f5ab2b
liyunjie12345 <wyz941231@gmail.com>
2025-02-19 11:59:25 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,14 @@
+/// week01-1.cpp 瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-S 存檔成 week01-1.cpp 要記得加 .cpp
+/// 下周考試考這題,不用寫藍色的中文註解
+
+#include <stdio.h>/// 檔名不能寫錯
+int main()
+{
+    int a, b;
+    scanf( "%d" , &a );
+    scanf( "%d" , &b );
+
+    printf("%d", a+b );
+}
