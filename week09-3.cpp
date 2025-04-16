734d1ad64b6687a5a09cdc6eb793bac6b5cb33d3
liyunjie12345 <wyz941231@gmail.com>
2025-04-16 12:12:06 +0800

    week09

---
+++ week09-3.cpp
@@ -0,0 +1,14 @@
+// week09-3.cpp
+// TEH001_Star
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for(int i=1; i<=n; i++){
+		for(int j=0; j<i; j++){
+			printf("*");
+		}
+		printf("%d: \n", i);
+	}
+}
